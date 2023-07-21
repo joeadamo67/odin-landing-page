9baf6d97b66167ee3ed2713cdd1c88d71dba2a7c
Joseph Adamo <140087801+joeadamo67@users.noreply.github.com>
2023-07-21 11:34:33 -0500

    Doing some exercises

--- javascript.js
+++ javascript.js
@@ -13,3 +13,32 @@ console.log(user+" is "+age+" years old");
 const dateOfBirth = "12.21.1900";
 
 console.log(dateOfBirth);
+
+const SPEED_OF_LIGHT = 299792458; //use caps for constants that are hard coded.
+
+//https://javascript.info/variables Tasks
+
+let admin;
+let name = "John";
+admin = name;
+
+//alert(admin); 
+
+const HOME_PLANET = "Earth";
+
+let currentVisitorName;
+
+
+//Odin Project Variable Tasks
+
+console.log(37+53);
+
+console.log((4+6+9) / 77);
+
+let a = 10;
+console.log(a);
+
+const max = 57;
+let actual = max-13;
+let percentage = actual/max;
+let remainder = actual%max;
