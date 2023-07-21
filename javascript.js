9b48d396dc620ce2ba33e5d16b29a063d91b4032
Joseph Adamo <140087801+joeadamo67@users.noreply.github.com>
2023-07-21 10:58:57 -0500

    Variables, console.logs, and alerts

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 console.log("Testing 123");
+
+let message;
+
+message = "Hello World";
+console.log(message);
+//alert(message); //stops program until clicked through
+
+let user = "John", age = 25;
+
+console.log(user+" is "+age+" years old");
+
+const dateOfBirth = "12.21.1900";
+
+console.log(dateOfBirth);
